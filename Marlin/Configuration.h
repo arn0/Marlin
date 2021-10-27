ce8b1da0705401eca24e27dffee26f27206c3e70
Arno <arnoc@xs4all.nl>
2021-10-27 20:26:41 +0200

    Correct for X endstop postition

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1376,7 +1376,7 @@
 #define Y_BED_SIZE 500
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -8
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
